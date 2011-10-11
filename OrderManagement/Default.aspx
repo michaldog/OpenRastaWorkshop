38c6a654246604f3e13a5f1b839064195ea89336
arneeiri <arne.eirik.nielsen@gyldendal.no>
2011-10-11 17:21:12 +0200

    added OpenRasta plumbing

---
+++ OrderManagement/Default.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page CodeFile="Default.aspx.cs" Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="OrderManagement.Default" %>
+<!-- Nothing to see here. -->
---
+++ OrderManagement/Default.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.SessionState;
+
+namespace OrderManagement
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
