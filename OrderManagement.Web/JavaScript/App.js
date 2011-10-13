054fc3e8e24c2801d6763b3cd754daa872c8e785
Torstein Bjørnstad <torstein.bjornstad@gyldendal.no>
2011-10-13 20:52:12 +0200

    Added orderlist

--- OrderManagement.Web/JavaScript/App.js
+++ OrderManagement.Web/JavaScript/App.js
@@ -4,15 +4,18 @@
     },
 
     initialize: function () {
-
+        this.orderListView = new OrderListView({ collection: orders, el: "#orderList" });
     },
 
     index: function () {
-        
+
     }
 });
 
-$(function () {
+$(function (window) {
+    var orders = new OrderList();
+    orders.fetch();
+    
     window.App = new App();
     Backbone.history.start();
 });
